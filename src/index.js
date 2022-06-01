fa3aee7b0e9ee8ff79dff3722c3b25153ca66836
kittipop <kittipco@ais.co.th>
2022-06-01 22:57:26 +0700

    done responsive search

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import axios from "axios";
 
-
-let content = require('./news/news_content.json');
+let content = {}
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -34,12 +34,13 @@ class Search extends React.Component{
     clearTimeout(this.timer);
     this.timer = setTimeout(() => {
       this.search();
-    }, 2000);
+    }, 1500);
   }
   
   search = () => {
     this.setState( prevState => ({ checked: !prevState.checked }));
     // send backend search request here ******
+    this.props.parentSearchMenu(this.state.text)
   }
   
   render () {
@@ -54,7 +55,7 @@ class Search extends React.Component{
 
 class Menu extends React.Component{
   
-  renderMenuButton(name){
+  renderMenuButton = (name) => {
     return <button  className='menu-button'
     value={name}
     onClick={() => {this.props.onClick(name);}}>
@@ -62,10 +63,14 @@ class Menu extends React.Component{
       </button>;
   }
 
+  handleSearchMenu = (text) => {
+    this.props.parentSearchAll(text)
+  }
+  
+
   render() {
 
     return (
-        
         <div className='menu'>
           <div className='logo-div'>
             <img className='logo' src={require('./img/logo.png')}/>
@@ -83,69 +88,118 @@ class Menu extends React.Component{
             {this.renderMenuButton('TV')}
           </div>
           <div className='menu-button-div'>
-            <Search />
+            <Search parentSearchMenu={this.handleSearchMenu}/>
           </div>
-          
-          
-          
-          
-          {/* {this.renderSearchBox()} */}
         </div>
       
     )
   }
 }
 
-class Content extends React.Component{
-  renderContent(fromMenu, elementOrder){
-    let img_path = require('./news/images/' + content[fromMenu][elementOrder].image)
-    return (
-      <div className='content-element'>
-        <div>
-          <div className='content-title'>
-            <h1>
-              {content[fromMenu][elementOrder].title}
-            </h1>
-          </div>
-          <div >
-            <img className='content-img' src={img_path}/>
-          </div>
+
+
+class ContentBody extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      fromMenu: "",
+      search: "",
+      contentInfo: {}
+    }
+  }
+
+  renderContent(contentInfo){
+    let e_key = Object.keys(contentInfo)
+    // console.log('e_key = ')
+    // console.log(e_key)
+    let elements = []
+    e_key.forEach(element => {
+      // console.log('element = ')
+      // console.log(element)
+      let img_path = require('./news/images/' + contentInfo[element].image)
+      elements.push(
+        <div className='content-element'>
           <div>
-            <p className='content-desc'>
-              {content[fromMenu][elementOrder].content}
-            </p>
+            <div className='content-title'>
+              <h1>
+                {contentInfo[element].title}
+              </h1>
+            </div>
+            <div >
+              <img className='content-img' src={img_path}/>
+            </div>
+            <div>
+              <p className='content-desc'>
+                {contentInfo[element].content}
+              </p>
+            </div>
+            <div className='content-update'>
+              <p>
+                Updated: {contentInfo[element].update}
+              </p>
+            </div>
+            
           </div>
-          <div className='content-update'>
-            <p>
-            Updated: {content[fromMenu][elementOrder].update}
-            </p>
-          </div>
-          
         </div>
+      )
+    });
+    // console.log('elements = ')
+    // console.log(elements)
+    return (
+      <div className='content-row'>
+        {elements}
+       
       </div>
     )
   }
 
   render() {
-
-    return (
-      <div>
-        <div className='content-row'>
-          {this.renderContent(this.props.dataFromMenu, "a")}
-          {this.renderContent(this.props.dataFromMenu, "b")}
-          {this.renderContent(this.props.dataFromMenu, "c")}
-          {this.renderContent(this.props.dataFromMenu, "d")}
-          {this.renderContent(this.props.dataFromMenu, "a")}
-          {this.renderContent(this.props.dataFromMenu, "b")}
-          {this.renderContent(this.props.dataFromMenu, "c")}
-          {this.renderContent(this.props.dataFromMenu, "d")}
-          {this.renderContent(this.props.dataFromMenu, "a")}
-          {this.renderContent(this.props.dataFromMenu, "b")}
-          {this.renderContent(this.props.dataFromMenu, "c")}
-          {this.renderContent(this.props.dataFromMenu, "d")}
+    console.log('this.props.dataFromMenu = ')
+    console.log(this.props.dataFromMenu)
+    console.log('this.props.searchFromMenu = ')
+    console.log(this.props.searchFromMenu)
+    let check = false
+    if (this.props.dataFromMenu != "") {
+      if (this.state.fromMenu !== this.props.dataFromMenu) {
+        let url = 'http://127.0.0.1:3001/main/content/' + this.props.dataFromMenu
+        // console.log('url = ')
+        // console.log(url)
+        axios.get(url).then(con => {
+          this.setState({
+            fromMenu: this.props.dataFromMenu,
+            search: "",
+            contentInfo: con.data
+          })})
+        console.log("wait response from backend for content type")
+        return (<div></div>)
+      } else {
+        check = true
+      }
+    } else if(this.props.searchFromMenu != "") {
+      if (this.state.search !== this.props.searchFromMenu) {
+        let url = 'http://127.0.0.1:3001/main/search/' + this.props.searchFromMenu
+        // console.log('url = ')
+        // console.log(url)
+        axios.get(url).then(con => {
+          this.setState({
+            fromMenu: "",
+            search: this.props.searchFromMenu,
+            contentInfo: con.data
+          })})
+        console.log("wait response from backend for content search")
+        return (<div></div>)
+      }else {
+        check = true
+      }
+    } 
+    if (check){
+      console.log("content state changed !!! => ")
+      return (
+        <div>
+          {this.renderContent(this.state.contentInfo)}
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
@@ -153,28 +207,41 @@ class All extends React.Component{
   constructor(props){
     super(props)
     this.state = {
+      search: "",
       content: "News"
     }
   }
-
 
   menuClick(name) {
     // change content of body to the clicked one
     console.log('click ' + name)
     this.setState({
+      search: "",
       content: name
     })
   }
 
-  render() {
+  handleState(s) {
+    this.setState(s)
+  }
 
+  handleSearchAll = (text) => {
+    console.log("search text at ALL = ")
+    console.log(text)
+    this.handleState({
+      search: text,
+      content: ""
+    })
+  }
+
+  render() {
     return (
       <div>
         
-        <Menu onClick={(name) => this.menuClick(name)}/>
+        <Menu onClick={(name) => this.menuClick(name)} parentSearchAll={this.handleSearchAll}/>
         
         <div className='content-c'>
-          <Content dataFromMenu={this.state.content}/>
+          <ContentBody dataFromMenu={this.state.content} searchFromMenu={this.state.search}/>
         </div>
       </div>
       
